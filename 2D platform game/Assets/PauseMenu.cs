2b8b49717426b7a8c14cb3791634db28dd7f02c3
Michal-Jackowski <michaljacko@op.pl>
2020-09-27 21:43:02 +0200

    UI Update

--- 2D platform game/Assets/PauseMenu.cs
+++ 2D platform game/Assets/PauseMenu.cs
@@ -7,6 +7,16 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    public GameObject settingsMenuUI;
+    public GameObject controlsMenuUI;
+    public GameObject graphicMenuUI;
+    public GameObject soundMenuUI;
+    public GameObject creditsMenuUI;
+    public GameObject LoadChapterMenu0UI;
+    public GameObject LoadChapterMenu1UI;
+    public GameObject LoadChapterMenu2UI;
+    public GameObject LoadChapterMenu3UI;
+    public GameObject LoadChapterMenu4UI;
 
 
     // Update is called once per frame
@@ -28,9 +38,18 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        settingsMenuUI.SetActive(false);
+        controlsMenuUI.SetActive(false);
+        graphicMenuUI.SetActive(false);
+        soundMenuUI.SetActive(false);
+        creditsMenuUI.SetActive(false);
+        LoadChapterMenu0UI.SetActive(false);
+        LoadChapterMenu1UI.SetActive(false);
+        LoadChapterMenu2UI.SetActive(false);
+        LoadChapterMenu3UI.SetActive(false);
+        LoadChapterMenu4UI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
-
     }
 
     void Pause()
--- 2D platform game/Assets/UI/Scripts/ChangeScene.cs
+++ 2D platform game/Assets/UI/Scripts/ChangeScene.cs
@@ -5,12 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public GameObject introMenuUI;
+    public GameObject startMenuUI;
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.anyKey)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            startMenuUI.SetActive(true);
+            introMenuUI.SetActive(false);
         }
     }
 }
