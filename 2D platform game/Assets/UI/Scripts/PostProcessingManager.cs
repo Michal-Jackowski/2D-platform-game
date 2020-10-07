96aa9ea75da55f7a9c523c28c5bf409a9713425f
Michal-Jackowski <michaljacko@op.pl>
2020-10-08 01:58:01 +0200

    UI Update

---
+++ 2D platform game/Assets/UI/Scripts/PostProcessingManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessingManager : MonoBehaviour
+{
+public PostProcessVolume volume;
+public float bloomValue;
+public GameObject introMenu;
+
+    void Update()
+    {
+        Bloom bloom;
+
+        if (introMenu.activeSelf == true)
+        {
+            if (volume.profile.TryGetSettings<Bloom>(out bloom))
+            {
+                bloom.intensity.value = bloomValue;
+                //Debug.Log("ActiveScene...");
+            }
+        }
+        else
+        {
+            if (volume.profile.TryGetSettings<Bloom>(out bloom))
+            {
+                bloom.intensity.value = 0.8f; //default value
+                //Debug.Log("Scene is not active...");
+                return;
+            }
+        }
+    }
+}
