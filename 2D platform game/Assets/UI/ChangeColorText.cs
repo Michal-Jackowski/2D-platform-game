cc9db47723c3519a617b3062d029306a926e557c
Michal-Jackowski <michaljacko@op.pl>
2020-10-14 23:48:13 +0200

    UI Update

--- 2D platform game/Assets/UI/ChangeColorText.cs
+++ 2D platform game/Assets/UI/ChangeColorText.cs
@@ -3,143 +3,164 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
+using System;
 
 public class ChangeColorText : MonoBehaviour
 {
-    //Menu scenes
+    [Header("Menu Scenes")]
     public GameObject settingMenu;
     public GameObject mainMenu;
 
-    //Menu firstSelectedObjects
+
+    [Header("Menu FirstSelectedObjects")]
     public GameObject settingMenuFirstSelectedButton; 
     public GameObject mainMenuFirstSelectedButton;
 
-    //Buttons
+
+    [Header("Main Menu Buttons")]
     public GameObject PlayButton;
     public GameObject LoadChapterButton;
+    public GameObject SettingsButton;
+    public GameObject ExitButton;
+
     
-    //MainMenu buttons
+    [Header("Main Menu Buttons Text")]
     public TextMeshProUGUI PlayButtonText;
     public TextMeshProUGUI LoadChapterButtonText;
     public TextMeshProUGUI SettingsButtonText;
     public TextMeshProUGUI ExitButtonText;
+
     
-    //SettingsMenu buttons
+    [Header("Settings Menu Buttons Text")]
     public TextMeshProUGUI ControlsButton;
     public TextMeshProUGUI GraphicButton;
     public TextMeshProUGUI SoundButton;
     public TextMeshProUGUI CreditsButton;
 
+
     GameObject currentSelected;
 
-    //bool settingMenuBegin = false;
+
     bool mainMenuBegin = false;
-    //bool firstSelectedGameObjectReady = false;
-    
+    bool settingMenuBegin = false;
+
+
     void Update()
     {
-        //MainMenu
-        if (mainMenu.activeSelf == true)
+        try
         {
-            //settingMenuBegin = false;
+            if (mainMenu.activeSelf == true)
+            {
+/*                 if(settingMenuBegin)
+                {
+                    EventSystem.current.SetSelectedGameObject(EventSystem.current.GetComponent<EventSystem>().currentSelectedGameObject);
+                } */
+                
+                if (!mainMenuBegin)
+                {
+                    ChangeAndSetFirstSelectedObject();
+                    mainMenuBegin = true;
+                }
+                else if (mainMenuBegin)
+                {
+                    currentSelected = EventSystem.current.currentSelectedGameObject;
+                }
 
-            if (!mainMenuBegin)
-            {
-            ChangeFirstSelectedObject();
-            //EventSystem.current.SetSelectedGameObject(EventSystem.current.GetComponent<EventSystem>().firstSelectedGameObject);
-            mainMenuBegin = true;
-            //firstSelectedGameObjectReady = true;
+                if(currentSelected.name == "PlayButton")
+                {
+                    PlayButtonText.color = new Color32(255, 255, 255, 255);
+                    LoadChapterButtonText.color = new Color32(100, 100, 100, 255);
+                    SettingsButtonText.color = new Color32(100, 100, 100, 255);
+                    ExitButtonText.color = new Color32(100, 100, 100, 255);
+                }
+                else if(currentSelected.name == "LoadChapterButton")
+                {
+                    PlayButtonText.color = new Color32(100, 100, 100, 255);
+                    LoadChapterButtonText.color = new Color32(255, 255, 255, 255);
+                    SettingsButtonText.color = new Color32(100, 100, 100, 255);
+                    ExitButtonText.color = new Color32(100, 100, 100, 255);
+                }
+                else if(currentSelected.name == "SettingsButton")
+                {
+                    PlayButtonText.color = new Color32(100, 100, 100, 255);
+                    LoadChapterButtonText.color = new Color32(100, 100, 100, 255);
+                    SettingsButtonText.color = new Color32(255, 255, 255, 255);
+                    ExitButtonText.color = new Color32(100, 100, 100, 255);
+                }
+                else if(currentSelected.name == "ExitButton")
+                {
+                    PlayButtonText.color = new Color32(100, 100, 100, 255);
+                    LoadChapterButtonText.color = new Color32(100, 100, 100, 255);
+                    SettingsButtonText.color = new Color32(100, 100, 100, 255);
+                    ExitButtonText.color = new Color32(255, 255, 255, 255);
+                }
+                else
+                {
+                    Debug.Log("Another state mainMenu");
+                }
             }
-            else if (mainMenuBegin) //&& firstSelectedGameObjectReady)
+            else if (settingMenu.activeSelf == true)
             {
-                currentSelected = EventSystem.current.currentSelectedGameObject;
-            }
+                mainMenuBegin = false;
+
+                if (!settingMenuBegin)
+                {
+                    ChangeAndSetFirstSelectedObject();
+                    settingMenuBegin = true;
+                }
+                else
+                {
+                    currentSelected = EventSystem.current.currentSelectedGameObject;
+                }
             
-            if(currentSelected.name == "PlayButton")
-            {
-                PlayButtonText.color = new Color32(255, 255, 255, 255);
-                LoadChapterButtonText.color = new Color32(100, 100, 100, 255);
-                ExitButtonText.color = new Color32(100, 100, 100, 255);
-            }
-            else if(currentSelected.name == "LoadChapterButton")
-            {
-                LoadChapterButtonText.color = new Color32(255, 255, 255, 255);
-                PlayButtonText.color = new Color32(100, 100, 100, 255);
-                SettingsButtonText.color = new Color32(100, 100, 100, 255);
-            }
-            else if(currentSelected.name == "SettingsButton")
-            {
-                SettingsButtonText.color = new Color32(255, 255, 255, 255);
-                LoadChapterButtonText.color = new Color32(100, 100, 100, 255);
-                ExitButtonText.color = new Color32(100, 100, 100, 255);
-            }
-            else if(currentSelected.name == "ExitButton")
-            {
-                ExitButtonText.color = new Color32(255, 255, 255, 255);
-                PlayButtonText.color = new Color32(100, 100, 100, 255);
-                SettingsButtonText.color = new Color32(100, 100, 100, 255);
-            }
-            else
-            {
-                Debug.Log("Another state mainMenu");
+                if(currentSelected.name == "ControlsButton")
+                {
+                    ControlsButton.color = new Color32(255, 255, 255, 255);
+                    GraphicButton.color = new Color32(100, 100, 100, 255);
+                    SoundButton.color = new Color32(100, 100, 100, 255);
+                    CreditsButton.color = new Color32(100, 100, 100, 255);
+                }
+                else if(currentSelected.name == "GraphicButton")
+                {
+                    ControlsButton.color = new Color32(100, 100, 100, 255);
+                    GraphicButton.color = new Color32(255, 255, 255, 255);
+                    SoundButton.color = new Color32(100, 100, 100, 255);
+                    CreditsButton.color = new Color32(100, 100, 100, 255);
+                }
+                else if(currentSelected.name == "SoundButton")
+                {
+                    ControlsButton.color = new Color32(100, 100, 100, 255);
+                    GraphicButton.color = new Color32(100, 100, 100, 255);
+                    SoundButton.color = new Color32(255, 255, 255, 255);
+                    CreditsButton.color = new Color32(100, 100, 100, 255);
+                }
+                else if(currentSelected.name == "CreditsButton")
+                {
+                    ControlsButton.color = new Color32(100, 100, 100, 255);
+                    GraphicButton.color = new Color32(100, 100, 100, 255);
+                    SoundButton.color = new Color32(100, 100, 100, 255);
+                    CreditsButton.color = new Color32(255, 255, 255, 255);
+                }
+                else
+                {
+                    Debug.Log("Another state settingMenu");
+                }
             }
         }
-        //SettingsMenu
-/*         else if (settingMenu.activeSelf == true)
-        {
-            mainMenuBegin = false;
-
-            if (!settingMenuBegin)
-            {
-                ChangeFirstSelectedObject();
-                EventSystem.current.SetSelectedGameObject(EventSystem.current.GetComponent<EventSystem>().firstSelectedGameObject);
-                settingMenuBegin = true;
-            }
-            else
-            {
-                currentSelected = EventSystem.current.currentSelectedGameObject;
-            }
-            
-            if(currentSelected.name == "ControlsButton")
-            {
-                ControlsButton.color = new Color32(255, 255, 255, 255);
-                GraphicButton.color = new Color32(100, 100, 100, 255);
-                CreditsButton.color = new Color32(100, 100, 100, 255);
-            }
-            else if(currentSelected.name == "GraphicButton")
-            {
-                GraphicButton.color = new Color32(255, 255, 255, 255);
-                ControlsButton.color = new Color32(100, 100, 100, 255);
-                SoundButton.color = new Color32(100, 100, 100, 255);
-            }
-            else if(currentSelected.name == "SoundButton")
-            {
-                SoundButton.color = new Color32(255, 255, 255, 255);
-                GraphicButton.color = new Color32(100, 100, 100, 255);
-                CreditsButton.color = new Color32(100, 100, 100, 255);
-            }
-            else if(currentSelected.name == "CreditsButton")
-            {
-                CreditsButton.color = new Color32(255, 255, 255, 255);
-                ControlsButton.color = new Color32(100, 100, 100, 255);
-                SoundButton.color = new Color32(100, 100, 100, 255);
-            }
-            else
-            {
-                Debug.Log("Another state settingMenu");
-            }
-        } */
+        catch (NullReferenceException) {}
     }
 
-    void ChangeFirstSelectedObject()
+    void ChangeAndSetFirstSelectedObject()
     {
         if (mainMenu.activeSelf == true)
         {
             EventSystem.current.GetComponent<EventSystem>().firstSelectedGameObject = mainMenuFirstSelectedButton;
+            EventSystem.current.SetSelectedGameObject(EventSystem.current.GetComponent<EventSystem>().firstSelectedGameObject);
         }
         else if (settingMenu.activeSelf == true)
         {
             EventSystem.current.GetComponent<EventSystem>().firstSelectedGameObject = settingMenuFirstSelectedButton;
+            EventSystem.current.SetSelectedGameObject(EventSystem.current.GetComponent<EventSystem>().firstSelectedGameObject);
         }
     }
 }
