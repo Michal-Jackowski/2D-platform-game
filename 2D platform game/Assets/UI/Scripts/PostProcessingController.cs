79985e4e30fa2920527b33e5f81fa90895a548dd
Michal-Jackowski <michaljacko@op.pl>
2020-10-22 10:48:52 +0200

    UI Update

---
+++ 2D platform game/Assets/UI/Scripts/PostProcessingController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessingController : MonoBehaviour
+{
+    public PostProcessVolume volume;
+    private Vignette vignette = null;
+    private Grain grain = null;
+
+    void Start () 
+    {
+        volume.profile.TryGetSettings(out vignette);
+        volume.profile.TryGetSettings(out grain);
+        InvokeRepeating("SetNewPostProcessingValues", Random.Range(1.0f, 3.0f), 0.1f);  //1s delay, repeat every 1s
+    }
+    void SetNewPostProcessingValues() 
+    {
+        vignette.smoothness.value = Random.Range(0.8f, 1.0f);
+        grain.intensity.value = Random.Range(0.35f, 0.65f);
+        grain.size.value = Random.Range(1.20f, 1.50f);
+    }
+}
