bcedcca7713665ac27e1a0d60f94afa718610bc1
Michal-Jackowski <michaljacko@op.pl>
2020-12-17 14:16:39 +0100

    PrototypeScene Update

---
+++ 2D platform game/Assets/CameraDistance.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using Cinemachine;
+using System.Collections;
+
+public class CameraDistance : MonoBehaviour
+{
+    public CinemachineVirtualCamera vcam;
+    int playerLayer;    //The layer the player game object is on
+    private float timeElapsed;
+    public BoxCollider2D cameraDistanceTrigger;
+    bool canChangeCameraDistance = false;
+    private float maxCameraDistance = 25f;
+
+    void Start()
+    {
+        //Get the integer representation of the "Player" layer
+		playerLayer = LayerMask.NameToLayer("Player");
+    }
+
+    void Update()
+    {
+        if(canChangeCameraDistance && vcam.GetCinemachineComponent<CinemachineFramingTransposer>().m_CameraDistance<=maxCameraDistance)
+        {
+            StartCoroutine(ExecuteAfterTime(0.02f));
+        }
+        else
+        {
+            canChangeCameraDistance = false;
+            cameraDistanceTrigger.enabled = true;
+        }
+    }
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		//If the collision wasn't with the player, exit
+		if (collision.gameObject.layer != playerLayer)
+			return;
+
+        canChangeCameraDistance = true;
+        cameraDistanceTrigger.enabled = false;
+	}
+
+    void AddCameraDistance(float cameraDistance)
+    {
+        vcam.GetCinemachineComponent<CinemachineFramingTransposer>().m_CameraDistance += cameraDistance;
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+ 
+        // Code to execute after the delay
+        AddCameraDistance(0.04f);
+    }
+}
